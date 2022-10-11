8b6bc54e8932596dc905719cef94927f597b4d5a
ErikBorgogno <erikborgo.20@gmail.com>
2022-10-11 17:50:58 +0200

    add new model and add relationship

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'slug', 'description'];
+    protected $fillable = ['title', 'slug', 'description', 'category_id'];
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
