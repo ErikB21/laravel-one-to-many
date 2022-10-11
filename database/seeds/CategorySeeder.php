9c0e07b889bacf82da65ba8a2684a7068a7caf03
ErikBorgogno <erikborgo.20@gmail.com>
2022-10-11 17:51:42 +0200

    add new migrate and seeder for categories, and update migratiojn posts

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Sport', 'Moda', 'Gossip', 'Motori', 'Cronaca', 'Cinema', 'Videogiochi', 'Musica'];
+
+        foreach($categories as $category){
+            $newCategory = new Category();
+            $newCategory->name = $category;
+            $newCategory->slug = Str::slug($category);
+            $newCategory->save();
+        }
+    }
+}
